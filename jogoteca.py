6ec85512c4ff0a6b57c26ea73912d7f0a729d009
Junior Wender <juniorwender1996@gmail.com>
2023-05-26 13:34:50 -0300

    user auth

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 class Game:
     def __init__(self, name, category, console):
@@ -12,22 +12,35 @@ game3 = Game('Mortal Kombat', 'Fight', 'PS2')
 game4 = Game('Skyrim', 'RPG', 'PC')
 list = [game1, game2, game3,game4]
 
+class User:
+    def __init__(self, id, name, password):
+        self.id = id
+        self.name = name
+        self.password = password
+
+user1 = User('1', 'admin', 'admin')
+user2 = User('2', 'user', 'user')
+users = {user1.id: user1, user2.id: user2}
+
+
 app = Flask(__name__)
 app.secret_key = 'criptografia'
 
 # ------------------------------------------------- Pages ----------------------------------------------------------------
 @app.route('/')
 def index():
-    # gamesList = [,'Skyrim','Minecraft','GTA V','The Witcher 3','Crash','God Of War','Valorant']
     return render_template('gameTable.html', title='Games', games=list)
 
 @app.route('/new')
 def new():
+    if 'logged_user' not in session or session['logged_user'] == None:
+        return redirect(url_for('login', next=url_for('create'))) # quary string
     return render_template('register.html', title='Register A New Game')
 
 @app.route('/login')
 def login():
-    return render_template('login.html', title='Login')
+    nextPage = request.args.get('next') # Captura o valor do quary string
+    return render_template('login.html', next=nextPage , title='Login')
 
 # ------------------------------------------------- Methods ----------------------------------------------------------------
 @app.route('/create', methods=['POST'])
@@ -37,24 +50,25 @@ def create():
     plataform = request.form['plataform']
     game = Game(name, category, plataform)
     list.append(game)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/auth', methods=['POST'])
 def auth():
-    user = request.form['user']
-    password = request.form['password']
-    if user == 'admin' and password == 'admin':
-        session['logged_user'] = request.form['user']
-        flash(session['logged_user'] + ' Logged successfully!')
-        return redirect('/')
+    if request.form['user'] in users:
+        user = users[request.form['user']]
+        if request.form['password'] == user.password:
+            session['logged_user'] = user.name
+            flash(user.name + ' Logged successfully!')
+            nextPage = request.form['next']
+            return redirect(nextPage)
     else:
         flash('Invalid user or password! Try again!')
-        return redirect('/login')
+        return redirect(url_for('login'))
     
 @app.route('/logout')
 def logout():
     session['logged_user'] = None
     flash('No user logged!')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 app.run(debug=True)
